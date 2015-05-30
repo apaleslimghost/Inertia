cfbbf66df0c07edb471d7da2652945964a726060
Matt Brennan <mattyb1000@gmail.com>
2015-05-30 23:12:51 +0100

    stuff, disamb

--- client/project.js
+++ client/project.js
@@ -76,9 +76,12 @@ function formatInterval(ival) {
 	return _.compact([
 		ival.hours() && _.padLeft(ival.hours(), 2, '0'),
 		ival.minutes() || '0',
-		ival.hours() ? false : _.padLeft(ival.seconds(), 2, '0')
+		ival.hours() ? false : _.padLeft(ival.seconds(), 2, '0') + 's'
 	]).join(':').split('').map(function(c) {
-		return '<span class="timechar' + (c === ':' ? ' timecolon' : '') + '">' + c + '</span>';
+		var extra = c === ':'? 'timecolon'
+		          : c === 's'? 'timesecond'
+		          : '';
+		return '<span class="timechar ' + extra + '">' + c + '</span>';
 	}).join('');
 }
 
